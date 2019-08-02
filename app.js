a98cf25ade1bbdea9c160ba761fb98652862c422
unknown <syedishmam17@gmail.com>
2019-08-02 19:03:55 -0400

    Change Counter to stateful

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ const App = (props) => {
             { props.initialPlayers.map( player => 
                 <Player
                     name= { player.name }
-                    score={ player.score }
                     key={ player.id.toString() }
                 />
             )}
@@ -56,19 +55,26 @@ const Player = (props) => {
             <span className="player-name">
                 { props.name }
             </span>
-            <Counter score={ props.score } />
+            <Counter />
         </div>
     );
 }
 
-const Counter = (props) => {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement">-</button>
-            <span className="counter-score">{ props.score }</span>
-            <button className="counter-action increment">+</button>
-        </div>
-    );
+class Counter extends React.Component {
+
+    state = {
+        score: 0
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement">-</button>
+                <span className="counter-score">{ this.state.score }</span>
+                <button className="counter-action increment">+</button>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
