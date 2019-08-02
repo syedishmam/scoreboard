d1b3095142e37a6b91c37560d156188a23f28255
unknown <syedishmam17@gmail.com>
2019-08-02 18:05:49 -0400

    Add App Player Header and Counter components

---
+++ app.js
@@ -0,0 +1,45 @@
+const App = () => {
+	return (
+		<div className="scoreboard">
+			<Header title="Scoreboard" totalPlayers={1}/>
+			{/* Player List */}
+			<Player />
+		</div>
+	);
+}
+
+const Header = () => {
+return (
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+    </header>
+);
+}
+
+
+const Player = () => {
+return (
+    <div className="player">
+        <span className="player-name">
+            Ish
+        </span>
+        <Counter />
+    </div>
+);
+}
+
+const Counter = () => {
+return (
+    <div className="counter">
+        <button className="counter-action decrement">-</button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment">+</button>
+    </div>
+);
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
